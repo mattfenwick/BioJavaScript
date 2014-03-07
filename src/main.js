ad58c2ce512f30aa3682f77cbf23f23d071e1d03
Matt Fenwick <mfenwick100@gmail.com>
2014-03-07 06:31:54 -0500

    put note about modules on html page, and make them all available through a single global property

--- src/main.js
+++ src/main.js
@@ -4,14 +4,16 @@ require.config({
     paths: {
 //        'jquery'    : '../bower_components/jquery/jquery',
 //        'HTML'      : '../bower_components/HTML/HTML',
-//        'unparse-js': '../bower_components/unparse-js/'
     }
 });
 
 var modules = ['aminoacid'];
 
 require(modules, function () {
+    var mods = {};
+    // make each module accessible through global object for convenience
     for (var i = 0; i < modules.length; i++) {
-        window[modules[i]] = arguments[i]; // add each module to global object for convenience
+        mods[modules[i]] = arguments[i];
     }
+    window.BJS = mods;
 });
